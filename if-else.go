f6c39cbc0d3f9987fd7d3b8c681989d7b1e47597
seregant <indraagus@live.com>
2019-03-12 10:10:48 +0700

    first commit

---
+++ if-else.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+	
+	num := 9
+	if num < 0 {
+        fmt.Println(num, "is negative")
+    } else if num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
+    }
+
+    fmt.Println(num)
+}
