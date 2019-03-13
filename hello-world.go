fe06a58e6d803185587b7350d21621772d7c97f1
seregant <indraagus@live.com>
2019-03-13 16:34:24 +0700

    test branch

--- hello-world.go
+++ hello-world.go
@@ -1,6 +1,8 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("hello world of fun")
-    fmt.Println("welcome to tcc lanjut")
+	fmt.Println("hello world of wonderful")
+	fmt.Println("welcome to tcc lanjut")
 }
